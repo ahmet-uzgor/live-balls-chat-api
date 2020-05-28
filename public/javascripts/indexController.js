460b5ec4bf69fc3bbe2fe346a0c963faf0e0b2c5
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-05-29 01:30:51 +0300

    Controller indexFactory is added

--- public/javascripts/indexController.js
+++ public/javascripts/indexController.js
@@ -1,6 +1,10 @@
-app.controller('indexController',['scope', ($scope)=>{
-    console.log('Controller');
-    const socket = io.connect("http://localhost:3000");
-    return ;
+app.controller('indexController',['scope','indexFactory', ($scope, indexFactory)=>{
+    indexFactory.connectSocket('http:localhost:3000',{
+        reconnectionAttempts: 3
+    }).then((socket)=>{
+        console.log('Connection is completed')
+    }).catch((err) =>{
+        console.log(err);
+    });
 }]);
 // controller kısmında not defined hatası veriyor
