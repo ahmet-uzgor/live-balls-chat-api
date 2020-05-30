6e06d9990289be734dc792906fe84dcdadf65423
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-05-30 20:14:10 +0300

    a new methods added to javascripts

--- public/javascripts/indexController.js
+++ public/javascripts/indexController.js
@@ -1,10 +1,20 @@
 app.controller('indexController',['scope','indexFactory', ($scope, indexFactory)=>{
-    indexFactory.connectSocket('http:localhost:3000',{
-        reconnectionAttempts: 3
-    }).then((socket)=>{
-        console.log('Connection is completed')
-    }).catch((err) =>{
-        console.log(err);
-    });
+
+    $scope.init = ()=>{
+        const username = prompt('Please enter a username');
+
+        if(username){
+            initSocket(username);
+        }
+    };
+    function initSocket(username){
+        indexFactory.connectSocket('http:localhost:3000',{
+            reconnectionAttempts: 3
+        }).then((socket)=>{
+            console.log('Connection is completed')
+        }).catch((err) =>{
+            console.log(err);
+        });
+    }
 }]);
 // controller kısmında not defined hatası veriyor
